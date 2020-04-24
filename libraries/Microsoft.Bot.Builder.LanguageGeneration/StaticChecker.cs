2ef7fa99a28a5ba581335b3015821a03ce6540fc
hond <hond@microsoft.com>
2020-04-24 10:33:40 +0800

    remove unused code

--- libraries/Microsoft.Bot.Builder.LanguageGeneration/StaticChecker.cs
+++ libraries/Microsoft.Bot.Builder.LanguageGeneration/StaticChecker.cs
@@ -3,12 +3,10 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using AdaptiveExpressions;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
-using Microsoft.Bot.Builder.LanguageGeneration.Events;
 
 namespace Microsoft.Bot.Builder.LanguageGeneration
 {
